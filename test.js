bb6f6df5da95ab8d99cbd8bbcadfd9a126a585a0
Oleg Pushenko <olegpushenko1998@gmail.com>
2019-01-23 01:03:10 +0200

    add file

---
+++ test.js
@@ -0,0 +1,6 @@
+var menuElem = document.getElementById('sweeties');
+    var titleElem = menuElem.querySelector('.title');
+
+    titleElem.onclick = function() {
+      menuElem.classList.toggle('open');
+    };
